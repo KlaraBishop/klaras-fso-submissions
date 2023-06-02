55c402ddf271d2af806ebeca9bdd067eadfcfb41
KleraStretch <stretch068@protonmail.com>
2023-06-01 23:32:00 -0700

    2.5

--- part2/course_information/src/App.jsx
+++ part2/course_information/src/App.jsx
@@ -1,30 +1,4 @@
-const Header = ({ name }) => {
-  return <h1>{name}</h1>
-}
-
-const Part = ({ part }) => {
-  return <p>{part.name} {part.exercises}</p>
-}
-
-const Content = ({ parts }) => {
-  return <>
-    {parts.map(part => <Part key={part.name} part={part} /> )} 
-  </>
-}
-
-const Total = ({ parts }) => {
-  const total = parts.reduce((sum, num) => sum += num.exercises, 0)
-
-  return <h4>Number of excercises: {total}</h4>
-}
-
-const Course = ({course}) => {
-  return <>
-    <Header name={course.name} />
-    <Content parts={course.parts} />
-    <Total parts={course.parts} />
-  </>   
-}
+import Course from "./components/course"
 
 const App = () => {
   const courses = [
