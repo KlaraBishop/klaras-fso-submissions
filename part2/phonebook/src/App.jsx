1a714d3c1da6c88474867ceaab224cb1fb6b5ab2
KleraStretch <stretch068@protonmail.com>
2023-06-04 23:58:49 -0700

    2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,16 +1,17 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Contacts from './components/contacts';
 import NewContactForm from './components/new-contact-form';
 import SearchBox from './components/search-box'; 
+import axios from 'axios';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-  const [filterValue, setFilterValue] = useState('') 
+  const [persons, setPersons] = useState([])
+  const [filterValue, setFilterValue] = useState('')
+  
+  useEffect (() => {
+    axios.get('http://localhost:8009/persons')
+    .then(res => setPersons(res.data))
+  },[])
 
   const filteredPersons = persons.filter(person => person.name.toLowerCase().includes(filterValue.toLowerCase()))
 
